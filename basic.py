b70c8ba2667bb61439c235be156bbefc9532c907
sec25ad167-cyber <sec25ad167@sairamtap.edu.in>
2026-03-10 21:24:26 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -9,3 +9,12 @@ print("Multiplication:", a * b)
 print("Division:", a / b)
 print("Whole Division:", a // b)
 print("Modulus:", a % b)
+import math
+
+x = float(input("Enter number: "))
+
+print("Square Root:", math.sqrt(x))
+print("Power (x^2):", x**2)
+print("Sin:", math.sin(x))
+print("Cos:", math.cos(x))
+print("Tan:", math.tan(x))
